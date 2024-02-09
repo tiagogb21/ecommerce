84b7731df76536af4abeae2ca8d8aae09f818dea
Tiago Garbi <tgbmateriais@gmail.com>
2024-02-09 15:56:19 -0300

    feature: config project livewire

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -14,10 +14,23 @@ class Category extends Model
 
     protected $fillable = [
         'name',
+        'slug',
+        'description',
+        'parent_id',
     ];
 
     public function products(): HasMany
     {
         return $this->hasMany(Product::class);
+    }
+
+    public function children(): HasMany
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
+    public function parent(): HasMany
+    {
+        return $this->hasMany(Category::class, 'id', 'parent_id');
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
-import './bootstrap';
+import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
+import checkout from "./checkout/checkout.js";
+import Clipboard from "@ryangjchandler/alpine-clipboard";
+
+Alpine.plugin(Clipboard);
+Alpine.data('checkout', checkout);
+Livewire.start();
