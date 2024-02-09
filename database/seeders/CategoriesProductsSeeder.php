9e60e6f9b88a308f0d5254833584780714106c7a
Tiago Garbi <tgbmateriais@gmail.com>
2024-02-09 16:30:53 -0300

    feature: create factories and seeders

---
+++ database/seeders/CategoriesProductsSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Brand;
+use App\Models\CategoriesProducts;
+use App\Models\Category;
+use App\Models\Feature;
+use App\Models\Sku;
+use Illuminate\Database\Seeder;
+
+class CategoriesProductsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+    }
+}
